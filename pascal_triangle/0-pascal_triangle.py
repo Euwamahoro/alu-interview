77a3f34c9518820c7e6707f15aa351d48331abee
Euwamahoro <e.uwamahoro@alustudent.com>
2024-02-23 01:38:15 +0200

    This is Enock

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n):
+    """
+    Generates Pascal's triangle up to the nth row.
+
+    Args:
+        n (int): The number of rows to generate in Pascal's triangle.
+
+    Returns:
+        list of lists: A list of lists of integers representing Pascal's triangle.
+                       Returns an empty list if n <= 0.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
+
+# Example usage:
+if __name__ == "__main__":
+    triangle = pascal_triangle(5)
+    for row in triangle:
+        print("[{}]".format(",".join(map(str, row))))
